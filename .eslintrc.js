91e8571599aa583cd6f83b9ac52cc175b2d705b6
Yuliia <kotyuliya24@gmail.com>
2024-07-20 11:24:38 -0400

    some changes

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
